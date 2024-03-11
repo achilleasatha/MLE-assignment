b1178323fb18eb8f9c48e0bc2ec8d03d29a1ef17
Achilleas Athanasiou Fragkoulis <achilleasatha@gmail.com>
2024-03-11 18:50:59 +0000

    Adding schemata for train, test data and inference

---
+++ prototype/data_schemata.py
@@ -0,0 +1,36 @@
+import pandera
+
+TRAINING_DATA_SCHEMA = pandera.DataFrameSchema(
+    {
+        "productId": pandera.Column(pandera.Int),
+        "gender": pandera.Column(pandera.String),
+        "description": pandera.Column(pandera.String),
+        "imageURL1": pandera.Column(pandera.String),
+        "imageURL2": pandera.Column(pandera.String),
+        "imageURL3": pandera.Column(pandera.String),
+        "imageURL4": pandera.Column(pandera.String),
+        "name": pandera.Column(pandera.String),
+        "productType": pandera.Column(pandera.String),
+        "pattern": pandera.Column(pandera.String),
+        "productIdentifier": pandera.Column(pandera.String),
+    }
+)
+
+TEST_DATA_SCHEMA = pandera.DataFrameSchema(
+    {
+        "productId": pandera.Column(pandera.Int),
+        "gender": pandera.Column(pandera.String),
+        "description": pandera.Column(pandera.String),
+        "imageURL1": pandera.Column(pandera.String),
+        "imageURL2": pandera.Column(pandera.String),
+        "imageURL3": pandera.Column(pandera.String),
+        "imageURL4": pandera.Column(pandera.String),
+        "name": pandera.Column(pandera.String),
+        "productType": pandera.Column(pandera.String),
+        "productIdentifier": pandera.Column(pandera.String),
+    }
+)
+
+INFERENCE_DATA_SCHEMA = pandera.SeriesSchema(
+    pandera.String, index={"name": "productIdentifier"}
+)
