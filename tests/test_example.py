49edc6c686ace20ff7c1d5bcca7e174f1d836135
Achilleas Athanasiou Fragkoulis <achilleasatha@gmail.com>
2024-03-11 18:50:20 +0000

    disabling reference tests

--- tests/test_example.py
+++ tests/test_example.py
@@ -4,6 +4,7 @@ import pytest
 
 
 @pytest.mark.parametrize("inputs, expected", [(1, 2), (3, 4)])
+@pytest.mark.skip(reason="Disable example tests")
 def test_example(inputs, expected):
     """A sample test :param inputs: :param expected: :return:"""
     assert inputs + 1 == expected
