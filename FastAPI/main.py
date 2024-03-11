76919b54b9bf75d03486cd396490fc4a70df65ea
Achilleas Athanasiou Fragkoulis <achilleasatha@gmail.com>
2024-03-11 18:13:52 +0000

    Add EDA notebooks and refactor API entrypoint

--- FastAPI/main.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.post("/infer")
-def infer(products: List[Product]):
-    #TODO: calculate predictons for given inputs and return the result
