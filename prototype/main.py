9405a4b8727911500b31b86bb4ab18062e6f1c2a
Achilleas Athanasiou Fragkoulis <achilleasatha@gmail.com>
2024-03-12 00:47:49 +0000

    Using same host as in config

--- prototype/main.py
+++ prototype/main.py
@@ -212,4 +212,4 @@ async def record_error_count(request: Request, call_next):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)  # nosec
